68bcb19d914ca5489ab208a144cdf195834ba72c
Wojciech Maj <45290820+Wojtas059@users.noreply.github.com>
2018-11-23 23:27:57 +0100

    Dodaje liczby zespolone

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+class Klasa{
+	private:
+		int liczby;
+	public:
+		std::string nazwa; 
+};
+
+
+int main(){
+	Klasa wojtas;
+	std::cout << "Podaj nazwe: " ;
+	std::cin >> nazwa.wojtas;
+	return 0;
+}
